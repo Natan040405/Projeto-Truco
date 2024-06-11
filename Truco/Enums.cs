e8080c347f0bb3e1e1e7f6bdb0dd4e2b1f98c85a
Natan Valente <113988166+Natan040405@users.noreply.github.com>
2024-06-11 07:33:17 -0300

    primeiro commit c#

---
+++ Truco/Enums.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Truco
+{
+    internal class Enums
+    {
+        public enum nipes
+        {
+            Copas = 1,
+            Ouro = 2,
+            Espadas = 3,
+            Paus = 4,
+        }
+
+        public enum classes
+        {
+            As = 1,
+            Dois = 2,
+            Tres = 3,
+            Quatro = 4,
+            Cinco = 5,
+            Seis = 6,
+            Sete = 7,
+            Dez = 8,
+            Valete = 9,
+            Dama = 10,
+            Rei = 11,
+            Coringa = 12,
+        }
+
+        public enum pontos
+        {
+            simples = 1,
+            Truco = 3,
+            Seis = 6,
+            Nove = 9,
+            Doze = 12,
+        }
+    }
+}
---
+++ Truco/carta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Truco
+{
+    internal class carta
+    {
+        public int nipe { get; set; }
+
+        public int classe { get; set; }
+    }
+}
